dab877a7c3839ed9a9f4b4fb9ec0d69210c1bae2
SE2Dev <brokensilencedev@gmail.com>
2016-03-29 15:29:26 -0400

    Additional Information

---
+++ issue1/issue1.c
@@ -0,0 +1,4 @@
+int main()
+{
+    printf("%s\n", "a string"); //These strings ARE highlighted correctly
+}
---
+++ issue1/issue1.cpp
@@ -0,0 +1,4 @@
+int main()
+{
+    printf("%s\n", "a string"); //These strings are NOT highlighted correctly
+}
